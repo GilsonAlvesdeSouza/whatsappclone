bd55cf629ea1bd2881773aa51587c64c0abd7081
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-01-20 18:20:35 -0300

    Sidebar Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import "./App.css";
+import { useState } from "react";
 
+import "./App.css";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
-import ChatIcon from '@material-ui/icons/Chat';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+
+import * as C from "./components";
 
 function App() {
+  const [chateList, setChateList] = useState([{}, {}, {}, {}]);
+
+  const handleChatList = () => {
+    return chateList.map((item, key) => (
+      <C.ChatListItem key={`chateList${key}}`} />
+    ));
+  };
+
   return (
     <div className="app-window">
       <div className="sidebar">
@@ -16,18 +28,26 @@ function App() {
           />
           <div className="header-buttons">
             <div className="header-btn">
-              <DonutLargeIcon style={{color: '#919191'}}/>
+              <DonutLargeIcon style={{ color: "#919191" }} />
             </div>
             <div className="header-btn">
-              <ChatIcon style={{color: '#919191'}}/>
+              <ChatIcon style={{ color: "#919191" }} />
             </div>
             <div className="header-btn">
-              <MoreVertIcon style={{color: '#919191'}}/>
+              <MoreVertIcon style={{ color: "#919191" }} />
             </div>
           </div>
         </header>
-        <div className="search">busca</div>
-        <div className="chatlist">chate</div>
+        <div className="search">
+          <div className="search-input">
+            <SearchIcon fontSize="small" style={{ color: "#919191" }} />
+            <input
+              type="search"
+              placeholder="Procurar ou começar uma nova conversa"
+            />
+          </div>
+        </div>
+        <div className="chatlist">{handleChatList()}</div>
       </div>
       <div className="content-area">bbbbbbb</div>
     </div>
---
+++ src/components/ChatListItem/index.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function ChatListItem() {
+    return (
+        <div>
+            ...
+        </div>
+    );
+}
+
+export default ChatListItem;
