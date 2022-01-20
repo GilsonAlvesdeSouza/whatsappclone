869a2f43ed6ef097137e5f4e086ec401d2fb5df8
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-01-20 16:58:53 -0300

    Divisão das interfaces sidebar e contentarea

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-window">
+      <div className="sidebar">aaaaaaaa</div>
+      <div className="content-area">bbbbbbb</div>
+    </div>
+  );
+}
+
+export default App;
