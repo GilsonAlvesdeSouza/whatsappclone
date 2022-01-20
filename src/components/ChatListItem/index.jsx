c777a926689fc684745e1ab5b825d27fc98bb6ef
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-01-20 20:11:16 -0300

    Sidebar ChatList (1/2)

--- src/components/ChatListItem/index.jsx
+++ src/components/ChatListItem/index.jsx
@@ -1,11 +1,25 @@
-import './style.css';
+import "./style.css";
 
 function ChatListItem() {
-    return (
-        <div>
-            ...
+  return (
+    <div className="chatlistitem">
+      <img
+        src="https://www.w3schools.com/howto/img_avatar2.png"
+        alt="avatar"
+      />
+      <div className="chatlistitem-lines">
+        <div className="chatlistitem-line">
+          <div className="chatlistitem-name">Gilson Alves</div>
+          <div className="chatlistitem-date">19:00</div>
         </div>
-    );
+        <div className="chatlistitem-line">
+          <div className="chatlistitem-lastmsg">
+            <p>Opa, tudo bem?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ChatListItem;
