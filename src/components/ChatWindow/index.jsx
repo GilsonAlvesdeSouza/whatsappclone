7b6fc580f3bd50918690d04632f5d23b4ede7898
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-02-16 13:21:35 -0300

    ContentArea (Chat Window Body)

--- src/components/ChatWindow/index.jsx
+++ src/components/ChatWindow/index.jsx
@@ -2,6 +2,10 @@ import "./style.css";
 import SearchIcon from "@material-ui/icons/Search";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import CloseIcon from "@material-ui/icons/Close";
+import SendIcon from "@material-ui/icons/Send";
+import MicIcion from "@material-ui/icons/Mic";
 
 function ChatWindow() {
   return (
@@ -28,7 +32,25 @@ function ChatWindow() {
         </div>
       </div>
       <div className="chatWindow--body"></div>
-      <div className="chatWindow--footer"></div>
+      <div className="chatWindow--footer">
+        <div className="chatWindow--pre">
+          <div className="chatWindow--btn">
+            <InsertEmoticonIcon style={{ color: "#919191" }} />
+          </div>
+        </div>
+        <div className="chatWindow--inputArea">
+          <input
+            className="chatWindow--input"
+            type="text"
+            placeholder="Digite uma mensagem"
+          />
+        </div>
+        <div className="chatWindow--pos">
+          <div className="chatWindow--btn">
+            <SendIcon style={{ color: "#919191" }} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
