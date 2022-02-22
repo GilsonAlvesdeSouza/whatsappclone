7c445d44a6dfa9f2d4df3da58a9c12dc388e9e30
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-02-22 16:07:01 -0300

    ContentArea (Emoji Area)

--- src/components/ChatWindow/index.jsx
+++ src/components/ChatWindow/index.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
 import "./style.css";
+import EmojiPicker, {
+  SKIN_TONE_MEDIUM_DARK,
+  SKIN_TONE_LIGHT,
+} from "emoji-picker-react";
 import SearchIcon from "@material-ui/icons/Search";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
@@ -8,6 +13,14 @@ import SendIcon from "@material-ui/icons/Send";
 import MicIcion from "@material-ui/icons/Mic";
 
 function ChatWindow() {
+  const [openEmoji, setOpenEmoji] = useState(false);
+
+  const handleEmojiClick = () => {};
+
+  const handleClickEmoji = () => {
+    setOpenEmoji(!openEmoji);
+  };
+
   return (
     <div className="ChatWindow">
       <div className="chatWindow--header">
@@ -32,9 +45,21 @@ function ChatWindow() {
         </div>
       </div>
       <div className="chatWindow--body"></div>
+      <div
+        className="chatWindow--emojiArea"
+        style={{ height: openEmoji ? "200px" : "0px" }}
+      >
+        <EmojiPicker
+          onEmojiClick={handleEmojiClick}
+          disableSearchBar
+          disableSkinTonePicker
+          activeSkinTone
+          skinTone={(SKIN_TONE_MEDIUM_DARK, SKIN_TONE_LIGHT)}
+        />
+      </div>
       <div className="chatWindow--footer">
         <div className="chatWindow--pre">
-          <div className="chatWindow--btn">
+          <div className="chatWindow--btn" onClick={handleClickEmoji}>
             <InsertEmoticonIcon style={{ color: "#919191" }} />
           </div>
         </div>
